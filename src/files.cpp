d2a73f4928ba30fcc6f0ea09b61232311b93e8b6
Cristian Di Pietrantonio <cristiandipietrantonio@gmail.com>
2025-08-23 21:18:25 +0800

    Implements function to list .dat files in a directory.

--- src/files.cpp
+++ src/files.cpp
@@ -3,6 +3,17 @@
 #include <sys/types.h>
 #include <string>
 #include <string.h>
+#include <vector>
+
+namespace {
+    bool ends_with(std::string const &full_string, std::string const &ending) {
+        if (full_string.length() >= ending.length()) {
+            return (0 == full_string.compare(full_string.length() - ending.length(), ending.length(), ending));
+        } else {
+            return false;
+        }
+    }
+}
 
 namespace blink {
 
@@ -34,6 +45,35 @@ namespace blink {
             } else {
                 return false;
             }
+        }
+        
+        /**
+        @brief list all the files in a directory.
+
+        @param path: path of a directory.
+
+        @returns a vector of string objects representing the path to all files
+        in the given directory.
+        */
+        std::vector<std::string> list_files_in_dir(std::string path, std::string ext){
+            DIR *dir;
+            struct dirent *ent;
+            std::vector<std::string> files;
+            if((dir = opendir(path.c_str())) != NULL) {
+                while((ent = readdir(dir)) != NULL) {
+                    std::string fname {ent->d_name};
+                    if(ext.length() > 0){
+                        if(::ends_with(fname, ext))
+                            files.push_back(path + "/" + fname);
+                    }else{
+                        files.push_back(fname);
+                    }
+                }
+                closedir (dir);
+            } else {
+                throw std::runtime_error {"list_files_in_dir: error while opening the directory " + path};
+            }
+            return files;
         }
     }
 }
