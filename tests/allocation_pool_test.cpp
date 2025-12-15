1cd039dbaf308504a7d7ba82ca57867a4cc0c334
Haralampos Protoolis <protoolish@gmail.com>
2025-12-15 12:59:13 +0800

    simplified test macros, merged some test cases together and added tests for `AllocationPool::cleanup`

--- tests/allocation_pool_test.cpp
+++ tests/allocation_pool_test.cpp
@@ -5,33 +5,23 @@
 #include "common.hpp"
 #include "../src/allocation_pool.hpp"
 
-void expect_expr(
-    const char* expression,
-    const char* func,
-    size_t lineno,
-    bool result
-) {
-    if (result) return;
+void expect_expr(const char* expr, const char* func, size_t line) {
+    std::string message = std::string{"FAILED: "} + expr;
 
-    std::string message = std::string{"FAILED: "} + expression;
-
-    if (message.size() > 80) message.resize(80);
-    else message.append(80 - message.size(), ' ');
+    if (message.size() < 80)
+        message.append(80 - message.size(), ' ');
 
     std::stringstream ss;
-    ss << message << " (" << func << ":" << lineno << ")\n";
+    ss << message << " (" << func << ":" << line << ")\n";
     throw TestFailed(ss.str());
 }
 
-#define EXPECT_TRUE(EXPRESSION) \
-    expect_expr(#EXPRESSION, __func__, __LINE__, EXPRESSION)
+#define EXPECT(EXPRESSION) \
+    if (!(EXPRESSION)) expect_expr(#EXPRESSION, __func__, __LINE__)
 
-#define EXPECT_FALSE(A) EXPECT_TRUE(!A)
-
-#define EXPECT_EQ(A, B) EXPECT_TRUE(A == B)
-
-#define EXPECT_NE(A, B) EXPECT_TRUE(A != B)
-
+/**
+ * @brief Helper function to fill the pool with unused buffers
+ */
 void fill_pool(AllocationPool& pool, const std::vector<size_t>& allocs) {
     std::vector<std::pair<char*, size_t>> mem;
 
@@ -44,38 +34,36 @@ void fill_pool(AllocationPool& pool, const std::vector<size_t>& allocs) {
     }
 }
 
-void test_single_alloc() {
-    constexpr size_t n = 16;
-
-    PageableAllocationPool pool{};
-    const char* ptr = pool.allocate(n);
-    EXPECT_NE(ptr, nullptr);
-    EXPECT_TRUE(pool.empty());
+std::ostream& operator<<(std::ostream& os, const AllocationPool& pool) {
+    os << "AllocationPool(total=" << pool.total_bytes() << ", "
+        << "unused=" << pool.unused_bytes() << ", "
+        << "max=" << pool.get_max_size() << ")";
+    return os;
 }
+
+// = TESTS =====================================================================
 
 void test_single_dealloc() {
     constexpr size_t n = 16;
 
     PageableAllocationPool pool{};
+
     char* ptr = pool.allocate(n);
-    EXPECT_EQ(pool.total_bytes(), n);
+
+    EXPECT(pool.total_bytes() == n);
+    EXPECT(pool.empty());
 
     // return to pool
     pool.deallocate(ptr, n);
-    EXPECT_FALSE(pool.empty());
-    EXPECT_EQ(pool.unused_bytes(), n);
-    EXPECT_EQ(pool.total_bytes(), n);
-}
 
-void test_single_reuse() {
-    constexpr size_t n = 16;
+    EXPECT(!pool.empty());
+    EXPECT(pool.unused_bytes() == n);
+    EXPECT(pool.total_bytes() == n);
 
-    PageableAllocationPool pool{};
-    char* ptr1 = pool.allocate(n);
-    // return to pool
-    pool.deallocate(ptr1, n);
     char* ptr2 = pool.allocate(n);
-    EXPECT_EQ(ptr1, ptr2);
+
+    EXPECT(ptr == ptr2);
+    EXPECT(pool.empty());
 }
 
 void test_tracking() {
@@ -86,13 +74,13 @@ void test_tracking() {
 
     fill_pool(pool, sizes);
 
-    EXPECT_EQ(pool.unused_bytes(), total);
-    EXPECT_EQ(pool.total_bytes(), total);
+    EXPECT(pool.unused_bytes() == total);
+    EXPECT(pool.total_bytes() == total);
 
     char* ptr = pool.allocate(64);
 
-    EXPECT_EQ(pool.unused_bytes(), total - 64);
-    EXPECT_EQ(pool.total_bytes(), total);
+    EXPECT(pool.unused_bytes() == total - 64);
+    EXPECT(pool.total_bytes() == total);
 }
 
 void test_max_size() {
@@ -103,18 +91,65 @@ void test_max_size() {
 
     fill_pool(pool, {n});
 
-    EXPECT_EQ(pool.unused_bytes(), n);
-    EXPECT_EQ(pool.total_bytes(), n);
+    EXPECT(pool.unused_bytes() == n);
+    EXPECT(pool.total_bytes() == n);
+
+    char* ptr = pool.allocate(1);
+
+    EXPECT(pool.unused_bytes() == n);
+    EXPECT(pool.total_bytes() == n + 1);
+
+    pool.deallocate(ptr, 1); // expecting that this will just get freed
+
+    EXPECT(pool.total_bytes() == n);
+}
+
+void test_cleanup_mostly_unused() {
+    PageableAllocationPool pool{};
+    pool.set_max_size(32);
+
+    fill_pool(pool, {16, 16});
+
+    EXPECT(pool.unused_bytes() == 32);
+    pool.cleanup();
+    EXPECT(pool.unused_bytes() == 32);
+
+    char* ptr = pool.allocate(8);
+    EXPECT(pool.unused_bytes() == 32);
+    EXPECT(pool.total_bytes() == 40);
+
+    EXPECT(pool.cleanup() == 16);
+
+    std::cout << pool << "\n";
+
+    EXPECT(pool.unused_bytes() == 16);
+    EXPECT(pool.total_bytes() == 24);
+}
+
+void test_cleanup_mostly_in_use() {
+    PageableAllocationPool pool{};
+    pool.set_max_size(32);
+
+    fill_pool(pool, {16, 16});
+
+    char* big_buf = pool.allocate(64);
+
+    EXPECT(pool.total_bytes() == 96);
+
+    EXPECT(pool.cleanup() == 32);
+
+    EXPECT(pool.unused_bytes() == 0);
+    EXPECT(pool.total_bytes() == 64);
 }
 
 int main() {
-    std::cout << "Starting AllocationPool Test" << std::endl;
+    std::cout << "Starting AllocationPool Tests" << std::endl;
     try {
-        test_single_alloc();
         test_single_dealloc();
-        test_single_reuse();
         test_tracking();
         test_max_size();
+        test_cleanup_mostly_unused();
+        test_cleanup_mostly_in_use();
     } catch (TestFailed ex) {
         std::cerr << ex.what() << std::endl;
         return 1;
